5d0d5f0d43ddea8e99abe60937c555cc42529625
Pankaj Kumar Ray <techyray35@gmail.com>
2026-02-07 12:30:41 +0530

    Download Brochure button added

--- src/components/hero/aboveTheFold/Ctas.jsx
+++ src/components/hero/aboveTheFold/Ctas.jsx
@@ -9,8 +9,15 @@ function Ctas(){
 fontColor="white" width="200px" />
 
 <span className="ter"></span>
-<Cta bgColor="white" content="Request On-Site Service Us"
-fontColor="#1FBAC6" width="306px" border="#1FBAC6" />
+
+<a className="downloadBrochure"
+href="https://res.cloudinary.com/aarzooray/image/upload/pandit_electricals_booklet_jkm_y1cwq6.pdf"
+  download="Pandit_Electricals_Brochure.pdf"
+  
+>
+    <span className="brohureIcon"><i class="ri-newspaper-line"></i></span>
+    <span href="https://res.cloudinary.com/aarzooray/raw/upload/pandit_electricals_booklet_jkm_y1cwq6.pdf" className="brochureLabel" download="https://res.cloudinary.com/aarzooray/raw/upload/pandit_electricals_booklet_jkm_y1cwq6.pdf">Download Brochure</span>
+</a>
 
         </div>
     )
